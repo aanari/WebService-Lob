7081eea9c772b27594bc3334b7498bd23c60c5c2
Ali Anari <ali@anari.me>
2014-08-22 14:14:35 -0600

    Added POD documentation

--- lib/WebService/Lob.pm
+++ lib/WebService/Lob.pm
@@ -52,4 +52,168 @@ method verify_address(
     return $result->{address};
 }
 
+# ABSTRACT: Lob API bindings
+
+=head1 SYNOPSIS
+
+    use WebService::Lob;
+
+    my $lob = WebService::NationBuilder->new( api_key => 'abc123' );
+
+    $lob->get_countries();
+
+=head1 DESCRIPTION
+
+This module provides bindings for the
+L<Lob|https://www.lob.com/docs> API.
+
+=head1 METHODS
+
+=head2 new
+
+Instantiates a new WebService::Lob client object.
+
+    my $lob = WebService::Lob->new(
+        api_key  => $api_key,
+        base_url => $domain,     # optional
+        timeout  => $retries,    # optional
+        retries  => $retries,    # optional
+    );
+
+B<Parameters>
+
+=over 4
+
+=item - C<api_key>
+
+I<Required>E<10> E<8>
+
+A valid Lob api key for your account.
+
+=item - C<base_url>
+
+I<Optional>E<10> E<8>
+
+The Lob base url to make API calls against.  Defaults to L<https://api.lob.com|https://api.lob.com>.
+
+=item - C<timeout>
+
+I<Optional>E<10> E<8>
+
+The number of seconds to wait per request until timing out.  Defaults to C<10>.
+
+=item - C<retries>
+
+I<Optional>E<10> E<8>
+
+The number of times to retry requests in cases when Lob returns a 5xx response.  Defaults to C<0>.
+
+=back
+
+=head2 get_states
+
+Returns a list of all US states.
+
+B<Request:>
+
+    get_states();
+
+B<Response:>
+
+    [{
+        name       => 'Alabama',
+        short_name => 'AL',
+        object     => 'state',
+    },
+    {
+        name       => 'Alaska',
+        short_name => 'AK',
+        object     => 'state',
+    },
+    ...
+    {
+        name       => 'Wisconsin',
+        short_name => 'WI',
+        object     => 'state',
+    },
+    {
+        name       => 'Wyoming',
+        short_name => 'WY',
+        object     => 'state',
+    }]
+
+=head2 get_countries
+
+Returns a list of all currently supported countries.
+
+B<Request:>
+
+    get_countries();
+
+B<Response:>
+
+    [{
+        name       => 'United States',
+        short_name => 'US',
+        object     => 'country',
+    },
+    {
+        name       => 'Afghanistan',
+        short_name => 'AF',
+        object     => 'country',
+    },
+    ...
+    {
+        name       => 'Zambia',
+        short_name => 'ZM',
+        object     => 'country',
+    },
+    {
+        name       => 'Zimbabwe',
+        short_name => 'ZW',
+        object     => 'country',
+    }]
+
+=head2 verify_address
+
+Validates an address given.
+
+B<Request:>
+
+    verify_address(
+        address_line1   => '370 Townsend St',
+        address_city    => 'San Francisco',
+        address_state   => 'CA',
+        address_zip     => '94107',
+        address_country => 'US',
+    );
+
+B<Response:>
+
+    {
+        object          => 'address',
+        address_line1   => '370 TOWNSEND ST',
+        address_line2   => '',
+        address_city    => 'SAN FRANCISCO',
+        address_state   => 'CA',
+        address_zip     => '94107-1607',
+        address_country => 'US',
+    }
+
+B<Exceptions:>
+
+=over 4
+
+=item - C<WebService::Lob::Exception::AddressNotFound>
+
+Address Not Found.
+
+=item - C<WebService::Lob::Exception::AddressMissingInformation>
+
+The address you entered was found but more information is needed to match to a specific address.
+
+=back
+
+=cut
+
 1;
--- lib/WebService/Lob/Exception/AddressNotFound.pm
+++ lib/WebService/Lob/Exception/AddressNotFound.pm
@@ -2,6 +2,6 @@ package WebService::Lob::Exception::AddressNotFound;
 use Moo;
 extends 'Throwable::Error';
 
-has '+message' => ( default => 'Address Not Found' );
+has '+message' => ( default => 'Address Not Found.' );
 
 1;
